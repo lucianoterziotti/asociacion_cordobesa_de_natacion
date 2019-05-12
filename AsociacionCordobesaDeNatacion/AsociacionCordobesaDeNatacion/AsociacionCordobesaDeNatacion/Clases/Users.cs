41743466039d7758551fe8a7d50be088396201dc
mebelenf1 <belenfassino@gmail.com>
2019-05-12 20:23:47 -0300

    funciona el login

--- AsociacionCordobesaDeNatacion/AsociacionCordobesaDeNatacion/AsociacionCordobesaDeNatacion/Clases/Users.cs
+++ AsociacionCordobesaDeNatacion/AsociacionCordobesaDeNatacion/AsociacionCordobesaDeNatacion/Clases/Users.cs
@@ -11,12 +11,33 @@ namespace AsociacionCordobesaDeNatacion
 {
     class Users
     {
+        int _id = 0;
+        string _usuario;
+        string _password;
+
+        public int id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string usuario
+        {
+            get { return _usuario; }
+            set { _usuario = value; }
+        }
+        public string password
+        {
+            get { return _password; }
+            set { _password = value; }
+
+        }
         AccesoBD _BD = new AccesoBD();
         
         public DataTable consulta_login(string usuario, string pssw)
         {
-            string sqltxt = @"SELECT * FROM users 
-                             WHERE n_usuario = '" + usuario + "'"
+            string sqltxt = @"SELECT * FROM Usuarios 
+                             WHERE nombre = '" + usuario + "'"
                              + " AND password = '" + pssw + "'";
 
             return _BD.consulta(sqltxt);
--- AsociacionCordobesaDeNatacion/AsociacionCordobesaDeNatacion/AsociacionCordobesaDeNatacion/Formularios/ABM-Clubes.Designer.cs
+++ AsociacionCordobesaDeNatacion/AsociacionCordobesaDeNatacion/AsociacionCordobesaDeNatacion/Formularios/ABM-Clubes.Designer.cs
@@ -37,34 +37,41 @@
             this.label4 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.cmd_nuevo = new System.Windows.Forms.Button();
             this.cmd_grabar = new System.Windows.Forms.Button();
             this.cmd_actualizar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // txt_nombre_usuario
             // 
             this.txt_nombre_usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_nombre_usuario.Location = new System.Drawing.Point(231, 152);
+            this.txt_nombre_usuario.Location = new System.Drawing.Point(115, 132);
+            this.txt_nombre_usuario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_nombre_usuario.MaxLength = 50;
             this.txt_nombre_usuario.Name = "txt_nombre_usuario";
-            this.txt_nombre_usuario.Size = new System.Drawing.Size(286, 26);
+            this.txt_nombre_usuario.Size = new System.Drawing.Size(323, 30);
             this.txt_nombre_usuario.TabIndex = 37;
             // 
             // txt_id
             // 
             this.txt_id.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_id.Location = new System.Drawing.Point(231, 120);
+            this.txt_id.Location = new System.Drawing.Point(115, 86);
+            this.txt_id.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_id.Name = "txt_id";
-            this.txt_id.Size = new System.Drawing.Size(51, 26);
+            this.txt_id.Size = new System.Drawing.Size(67, 30);
             this.txt_id.TabIndex = 36;
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.txt_nombre_usuario);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.txt_id);
             this.groupBox1.Controls.Add(this.btn_buscar);
             this.groupBox1.Controls.Add(this.groupBox2);
             this.groupBox1.Controls.Add(this.cmd_nuevo);
@@ -73,9 +80,11 @@
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(55, 45);
+            this.groupBox1.Location = new System.Drawing.Point(30, 38);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(671, 261);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(947, 334);
             this.groupBox1.TabIndex = 39;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "CLUBES";
@@ -83,9 +92,10 @@
             // btn_buscar
             // 
             this.btn_buscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_buscar.Location = new System.Drawing.Point(233, 75);
+            this.btn_buscar.Location = new System.Drawing.Point(198, 85);
+            this.btn_buscar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_buscar.Name = "btn_buscar";
-            this.btn_buscar.Size = new System.Drawing.Size(85, 25);
+            this.btn_buscar.Size = new System.Drawing.Size(113, 31);
             this.btn_buscar.TabIndex = 32;
             this.btn_buscar.Text = "Buscar";
             this.btn_buscar.UseVisualStyleBackColor = true;
@@ -98,9 +108,11 @@
             this.groupBox2.Controls.Add(this.textBox1);
             this.groupBox2.Controls.Add(this.label1);
             this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox2.Location = new System.Drawing.Point(89, 139);
+            this.groupBox2.Location = new System.Drawing.Point(6, 164);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(373, 97);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(462, 119);
             this.groupBox2.TabIndex = 31;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Domicilio";
@@ -108,68 +120,53 @@
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(305, 40);
+            this.textBox2.Location = new System.Drawing.Point(376, 49);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.MaxLength = 50;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(43, 26);
+            this.textBox2.Size = new System.Drawing.Size(56, 30);
             this.textBox2.TabIndex = 14;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(243, 46);
+            this.label4.Location = new System.Drawing.Point(300, 57);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(56, 16);
+            this.label4.Size = new System.Drawing.Size(68, 20);
             this.label4.TabIndex = 23;
             this.label4.Text = "Número";
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(82, 40);
+            this.textBox1.Location = new System.Drawing.Point(109, 49);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.MaxLength = 50;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(147, 26);
+            this.textBox1.Size = new System.Drawing.Size(172, 30);
             this.textBox1.TabIndex = 14;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(33, 46);
+            this.label1.Location = new System.Drawing.Point(44, 57);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(39, 16);
+            this.label1.Size = new System.Drawing.Size(47, 20);
             this.label1.TabIndex = 23;
             this.label1.Text = "Calle";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(114, 81);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 16);
-            this.label2.TabIndex = 17;
-            this.label2.Text = "Código";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(104, 113);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(57, 16);
-            this.label3.TabIndex = 23;
-            this.label3.Text = "Nombre";
             // 
             // cmd_nuevo
             // 
             this.cmd_nuevo.BackgroundImage = global::AsociacionCordobesaDeNatacion.Properties.Resources.nuevo1;
             this.cmd_nuevo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.cmd_nuevo.Location = new System.Drawing.Point(542, 0);
+            this.cmd_nuevo.Location = new System.Drawing.Point(773, 0);
+            this.cmd_nuevo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmd_nuevo.Name = "cmd_nuevo";
-            this.cmd_nuevo.Size = new System.Drawing.Size(37, 35);
+            this.cmd_nuevo.Size = new System.Drawing.Size(49, 43);
             this.cmd_nuevo.TabIndex = 25;
             this.cmd_nuevo.Tag = "Condiciones para crear un nuevo registro";
             this.cmd_nuevo.UseVisualStyleBackColor = true;
@@ -180,9 +177,10 @@
             this.cmd_grabar.BackgroundImage = global::AsociacionCordobesaDeNatacion.Properties.Resources.grabar1;
             this.cmd_grabar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.cmd_grabar.Enabled = false;
-            this.cmd_grabar.Location = new System.Drawing.Point(585, 0);
+            this.cmd_grabar.Location = new System.Drawing.Point(830, 0);
+            this.cmd_grabar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmd_grabar.Name = "cmd_grabar";
-            this.cmd_grabar.Size = new System.Drawing.Size(37, 35);
+            this.cmd_grabar.Size = new System.Drawing.Size(49, 43);
             this.cmd_grabar.TabIndex = 26;
             this.cmd_grabar.UseVisualStyleBackColor = true;
             // 
@@ -191,28 +189,59 @@
             this.cmd_actualizar.BackgroundImage = global::AsociacionCordobesaDeNatacion.Properties.Resources.icons8_actualizar_48;
             this.cmd_actualizar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.cmd_actualizar.Enabled = false;
-            this.cmd_actualizar.Location = new System.Drawing.Point(628, 0);
+            this.cmd_actualizar.Location = new System.Drawing.Point(887, 0);
+            this.cmd_actualizar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmd_actualizar.Name = "cmd_actualizar";
-            this.cmd_actualizar.Size = new System.Drawing.Size(37, 35);
+            this.cmd_actualizar.Size = new System.Drawing.Size(49, 43);
             this.cmd_actualizar.TabIndex = 27;
             this.cmd_actualizar.UseVisualStyleBackColor = true;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 93);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 20);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Código";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 132);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 20);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Nombre";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(491, 86);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(438, 197);
+            this.dataGridView1.TabIndex = 40;
+            // 
             // ABM_Clubes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(765, 361);
-            this.Controls.Add(this.txt_nombre_usuario);
-            this.Controls.Add(this.txt_id);
+            this.ClientSize = new System.Drawing.Size(1029, 397);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ABM_Clubes";
             this.Text = "ABM_Clubes";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -231,5 +260,6 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btn_buscar;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
